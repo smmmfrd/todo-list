6c6817ad2f8c27398863172ef971d02087a94517
Sam Mumford <smmmfrd@gmail.com>
2022-07-11 10:17:17 -0700

    Modal Functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import './style.css';
+const content = document.querySelector('#content');
 
-alert('sound the alarm');
+function buildModal(){
+    let modal = document.createElement('dialog');
+    modal.classList.add('modal');
+
+    let closeButton = document.createElement('button');
+    closeButton.addEventListener('click', () => modal.close());
+    closeButton.textContent = 'close';
+    modal.appendChild(closeButton);
+
+    let inputs = document.createElement('p');
+
+    let nameLabel = document.createElement('label');
+    nameLabel.setAttribute('for', 'name');
+    nameLabel.innerHTML = 'Name :';
+    inputs.appendChild(nameLabel);
+    let nameInput = document.createElement('input');
+    nameInput.type = 'text';
+    nameInput.id = 'name';
+    inputs.appendChild(nameInput);
+
+    modal.appendChild(inputs);
+
+    let submitButton = document.createElement('button');
+    submitButton.addEventListener('click', () =>{
+        console.log(`heyo`);
+    });
+    submitButton.textContent = 'Add';
+    modal.appendChild(submitButton);
+
+    content.appendChild(modal);
+
+    let showButton = document.createElement('button');
+    showButton.addEventListener('click', () => modal.showModal());
+    showButton.textContent = 'open';
+    content.appendChild(showButton);
+}
+
+buildModal();
